444cc4b4a409464560632f2874ad6b7fee7dc90d
Eugenio Gimenez <e.o.gimenez@gmail.com>
2023-02-10 18:24:57 +0100

    lab solved

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1:27017/recipes-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,38 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    return Recipe.create ({
+      title: "curry",
+      level: "Easy Peasy",
+      ingredients: ["pollo", "patata", "nata", "cebolla"],
+      cuisine: "Oriente",
+      dishType: "main_course",
+      image: "https://images.media-allrecipes.com/userphotos/720x405/815964.jpg",
+      duration: 40,
+      create: "EoG"
+    })
+  })
+  .then((res) => {
+    console.log(`**Recipe add:${res.title}`)
+    return Recipe.insertMany(data);
+  })
+  .then((result) => {
+    result.forEach((res) => {
+    console.log(`** Recipie title: ${res.title}`)
+    })
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"}, {duration: 100}, {new: true});
+  }) 
+  .then(() => {
+    console.log("*Changed success*")
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then (() => {
+    console.log("*Deleted success*")
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
+  })
+  .finally(() => {
+    console.log("*** conexion cerrada! ***")
+    mongoose.connection.close()
   });
